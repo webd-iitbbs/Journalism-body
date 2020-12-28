10ff08f306c06b5127d8d4b0194a86aa3e469331
Sudheer Bulusu <bds15@iitbbs.ac.in>
2020-12-28 18:33:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-// app.listen(process.env.PORT || 3000, function(){
-//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 MongoClient.connect('mongodb+srv://ja123:ja123@cluster0.k3ytz.mongodb.net/ja-articles?retryWrites=true&w=majority'
 ,{
